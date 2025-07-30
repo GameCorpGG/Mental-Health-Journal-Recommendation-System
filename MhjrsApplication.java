ba549e3779c5b73736ed129b9d38c1bd1241d4e1
Rachit Sen <167679033+GameCorpGG@users.noreply.github.com>
2025-07-30 11:44:25 +0530

    Add files via upload

---
+++ MhjrsApplication.java
@@ -0,0 +1,12 @@
+package com.Rachit.mental_health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhjrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MhjrsApplication.class, args);
+    }
+}
